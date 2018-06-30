a4d2fa7b647b8f2057fee5ffec3e047072fbb9b8
KevinLin <kevinlin0638@gmail.com>
2018-06-30 11:48:34 +0800

    load image

--- __main__.py
+++ __main__.py
@@ -1,4 +1,16 @@
+# Standard scientific Python imports
 import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
+
+# The digits dataset
+digits = datasets.load_digits()
+
+# 讀取所需圖片
+images_and_labels = list(zip(digits.images, digits.target))
+for index, (image, label) in enumerate(images_and_labels[:4]):
+    plt.subplot(2, 4, index + 1)
+    plt.axis('off')
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title('Training: %i' % label)
